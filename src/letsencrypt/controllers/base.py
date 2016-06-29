b28328a94a3783d356a832d393eb679321858ce4
joamag <joamag@hive.pt>
2016-06-29 13:12:45 +0100

    better path creation

--- src/letsencrypt/controllers/base.py
+++ src/letsencrypt/controllers/base.py
@@ -1,6 +1,7 @@
 #!/usr/bin/python
 # -*- coding: utf-8 -*-
 
+import os
 import json
 import base64
 import hashlib
@@ -9,12 +10,10 @@ import appier
 
 class BaseController(appier.Controller):
 
-    @appier.route("/", "GET")
-    def index(self):
+    @appier.route("/.well-known/acme-challenge/<str:token>", "GET")
+    def challenge(self, token):
         thumbprint = self.jwk_thumbprint()
-        return dict(
-            thumbprint = thumbprint
-        )
+        return "%s.%s" % (token, thumbprint)
 
     def jwk_thumbprint(self, account_key = None):
         account_key = account_key or self._account_key()
@@ -25,8 +24,18 @@ class BaseController(appier.Controller):
         result = hash.digest()
         return self._jose_b64(result)
 
-    def _account_key(self, encoding = "utf-8"):
-        file = open("C:/repo.extra/letsencrypt/src/letsencrypt/private_key.json", "rb")
+    def _account_key(self, account = None, encoding = "utf-8"):
+        le_path = appier.conf("LE_PATH", "/etc/letsencrypt")
+        accounts_path = os.path.join(
+            le_path,
+            "accounts",
+            "acme-v01.api.letsencrypt.org",
+            "directory"
+        )
+        if not account: account = os.listdir(accounts_path)[0]
+        account_path = os.path.join(accounts_path, account)
+        file_path = os.path.join(account_path, "private_key.json")
+        file = open(file_path, "rb")
         try: data = file.read()
         finally: file.close()
         data = data.decode(encoding)
